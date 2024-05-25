890606b9636884755b0bc1adc115d63bf459ba94
Pulkit <pulkit0707chawla@gmail.com>
2024-05-25 12:30:34 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,44 +1,44 @@
 import React, { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
+
 
 export default function Signin() {
-  const [fromData,setFormData]=useState({});
-  const [error,setError]=useState(null);
-  const [loading,setLoading]=useState(null);
+  const [formData,setFormData]=useState({});
+  const {loading,error}=useSelector((state)=>state.user);
   const navigate=useNavigate();
+  const dispatch=useDispatch();
   const handleChange=(e)=>{
     setFormData({
-      ...fromData,
+      ...formData,
       [e.target.id]: e.target.value,
     })
   }
   const handleSubmit=async (e)=>{
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatch(signInStart());
     const res=await fetch('/api/auth/signin',{
       method: 'POST',
       headers:{
         'Content-Type':'application/json',
       },
-      body: JSON.stringify(fromData),
+      body: JSON.stringify(formData),
     });
     const data=await res.json();
     if(data.success===false){
-      setError(data.message);
-      setLoading(false);
+      dispatch(signInFailure(data.message));
       return;
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
     }catch(err){
-      setLoading(false);
-      setError(err.message);
+      dispatch(signInFailure(err.message));
     }
     console.log(data);
   }
-  console.log(fromData);
+  console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl text-center font-semibold my-7'>Signin</h1>
