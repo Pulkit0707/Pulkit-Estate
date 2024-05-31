55cefed0c08d61d4d37c202ddb7548129d1e6155
Pulkit <pulkit0707chawla@gmail.com>
2024-05-31 12:48:08 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose.connect("mongodb+srv://pulkit0707chawla:hjYdcSqb3ot3ItwC@pulkit-estate.
 }).catch((err)=>{
     console.log(err);
 });
+
+const __dirname=path.resolve();
 
 const app=express();
 
@@ -30,6 +33,12 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 //middleware to optimize errors
 app.use((err,req,res,next)=>{
